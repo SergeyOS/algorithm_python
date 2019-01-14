dcef79c198185cb8924fb8fcdfbba67d4019ea08
SergeyOS <ad86@yandex.ru>
2019-01-14 22:08:00 +0500

    2 задания к "Урок 4. Эмпирическая оценка алгоритмов на Python"

---
+++ task_1.py
@@ -0,0 +1,102 @@
+﻿# 8. Посчитать, сколько раз встречается определенная цифра в введенной последовательности чисел.
+# Количество вводимых чисел и цифра, которую необходимо посчитать, задаются вводом с клавиатуры.
+import random
+import cProfile
+
+SIZE = 1000000
+MIN_ITEM = 0
+MAX_ITEM = 1000
+
+
+def init_array(size: int):
+    return [random.randint(MIN_ITEM, MAX_ITEM * size) for _ in range(size)]
+
+
+# 1. Алгоритм с рекурсией
+
+def count_rec(size: int):
+    enter_numbers = init_array(size)  # имитация ввода чисел
+    enter_element = random.randint(0, 9)  # имитация ввода цифры
+
+    # print(f'Исходный массив：{enter_numbers}. Считаем {enter_element}')
+    def count_in_value(value: int, element: int) -> int:
+        i = 0
+        if value % 10 == element:
+            i = 1
+
+        if value // 10 == 0:
+            return i
+        else:
+            return i + count_in_value(value // 10, element)
+
+    result_count = 0
+    for i in range(len(enter_numbers)):
+        result_count += count_in_value(enter_numbers[i], enter_element)
+
+    return result_count
+
+
+# 100 loops, best of 5: 253 usec per loop -     100
+# 100 loops, best of 5: 2.87 msec per loop -   1000
+# 100 loops, best of 5: 31.8 msec per loop -  10000
+# 100 loops, best of 5: 341 msec per loop -  100000
+# 100 loops, best of 5: 3.76 sec per loop - 1000000
+# cProfile.run('count_rec(100000)')
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.002    0.002    0.505    0.505 <string>:1(<module>)
+#    100001    0.066    0.000    0.143    0.000 random.py:174(randrange)
+#    100001    0.029    0.000    0.172    0.000 random.py:218(randint)
+#    100001    0.052    0.000    0.077    0.000 random.py:224(_randbelow)
+#         1    0.000    0.000    0.202    0.202 task3.py:11(init_array)
+#         1    0.030    0.030    0.202    0.202 task3.py:12(<listcomp>)
+#         1    0.027    0.027    0.503    0.503 task3.py:17(count_rec)
+# 788844/100000    0.274    0.000    0.274    0.000 task3.py:22(count_in_value) # Рекурсивный вызов
+#         1    0.001    0.001    0.506    0.506 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+#    100001    0.007    0.000    0.007    0.000 {method 'bit_length' of 'int' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+#    134563    0.018    0.000    0.018    0.000 {method 'getrandbits' of '_random.Random' objects}
+
+
+# 1. Алгоритм с циклом
+
+def count_loop(size: int):
+    enter_numbers = init_array(size)  # имитация ввода чисел
+    enter_element = random.randint(0, 9)  # имитация ввода цифры
+    # print(f'Исходный массив：{enter_numbers}. Считаем {enter_element}')
+    result_count = 0
+    for i in range(len(enter_numbers)):
+        value = enter_numbers[i]
+        while value // 10 != 0:
+            if value % 10 == enter_element:
+                result_count += 1
+            value = value // 10
+
+    return result_count
+# 100 loops, best of 5: 190 usec per loop  -     100
+# 100 loops, best of 5: 2.04 msec per loop -    1000
+# 100 loops, best of 5: 23.3 msec per loop -   10000
+# 100 loops, best of 5: 249 msec per loop  -  100000
+# 100 loops, best of 5: 2.65 sec per loop  - 1000000
+# cProfile.run('count_loop(100000)')
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#      1    0.002    0.002    0.325    0.325 <string>:1(<module>)
+# 100001    0.068    0.000    0.145    0.000 random.py:174(randrange)
+# 100001    0.030    0.000    0.175    0.000 random.py:218(randint)
+# 100001    0.052    0.000    0.077    0.000 random.py:224(_randbelow)
+#      1    0.000    0.000    0.208    0.208 task3.py:11(init_array)
+#      1    0.033    0.033    0.208    0.208 task3.py:12(<listcomp>)
+#      1    0.115    0.115    0.323    0.323 task3.py:60(count_for)
+#      1    0.001    0.001    0.326    0.326 {built-in method builtins.exec}
+#      1    0.000    0.000    0.000    0.000 {built-in method builtins.len}
+# 100001    0.007    0.000    0.007    0.000 {method 'bit_length' of 'int' objects}
+#      1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+# 134544    0.018    0.000    0.018    0.000 {method 'getrandbits' of '_random.Random' objects}
+
+
+# Вывод:
+# Алгоритм с рекурсией более медленный и забивает стек. При этом средняя скорость и алгоритма с рекурсией
+# и с алгоритма с циклом  прямо пропорциональна размеру массива.
+
+# print(count_rec(100000))
+# print(count_loop(100000))
---
+++ task_2.py
@@ -0,0 +1,107 @@
+﻿#  2. Написать два алгоритма нахождения i-го по счёту простого числа.
+#  Без использования «Решета Эратосфена»;
+#  Используя алгоритм «Решето Эратосфена»
+import cProfile
+
+MAX_LIMIT_NUMBER = 1000000
+KOEFF_ARRAY = 1000 # костыль, решающий проблему с ограничением массива
+
+
+# 1. Используя алгоритм «Решето Эратосфена»
+def sieve(num):
+    if num <= 0:
+        return -1
+    elif num >= MAX_LIMIT_NUMBER:
+        return -1
+    size = num * KOEFF_ARRAY
+    spam_list = [True] * size
+    spam_list[0] = False
+    spam_list[1] = False
+    m = 2
+    num_simple_number = 0
+    while m < size:
+        if spam_list[m]:
+            j = m * 2
+            num_simple_number += 1
+            if num == num_simple_number:
+                return m
+            while j < size:
+                spam_list[j] = False
+                j = j + m
+        m += 1
+    else:
+        return -1
+
+
+# 100 loops, best of 5: 1.46 msec per loop -  10
+# 100 loops, best of 5: 21 msec per loop   - 100
+# 100 loops, best of 5: 131 msec per loop  -  500
+# 100 loops, best of 5: 293 msec per loop - 1000
+# 100 loops, best of 5: 3.36 sec per loop - 9999
+# cProfile.run('sieve(1000)')
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.003    0.003    0.281    0.281 <string>:1(<module>)
+#         1    0.278    0.278    0.278    0.278 task2.py:11(sieve)
+#         1    0.000    0.000    0.281    0.281 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+# 2.  Без использования «Решета Эратосфена»;
+def no_sieve(num):
+    if num <= 0:
+        return -1
+    elif num >= MAX_LIMIT_NUMBER:
+        return -1
+    size = num * KOEFF_ARRAY
+    num_simple_number = 0
+    for spam in range(2, size):
+        eggs = 2
+        while eggs * eggs <= spam:
+            if spam % eggs == 0:
+                break
+            eggs += 1
+        else:
+            num_simple_number += 1
+            if num == num_simple_number:
+                return spam
+    return -1
+
+
+# 100 loops, best of 5: 6.4 usec per loop - 10
+# 100 loops, best of 5: 270 usec per loop - 100
+# 100 loops, best of 5: 3.45 msec per loop - 500
+# 100 loops, best of 5: 10.1 msec per loop - 1000
+# 100 loops, best of 5: 356 msec per loop   - 9999
+
+# cProfile.run('no_sieve(1000)')
+# ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.000    0.000    0.011    0.011 <string>:1(<module>)
+#         1    0.011    0.011    0.011    0.011 task2.py:55(no_sieve)
+#         1    0.000    0.000    0.011    0.011 {built-in method builtins.exec}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+
+
+# Выводы:
+# Алгоритм без использования «Решета Эратосфена» работает быстрее на относительно малых числах. Но с увеличением
+# порядкового номера искомого числа среднее время поиска растет нелинейно.
+# В то время как в алгоритме с использованием «Решета Эратосфена»
+# среднее время выполнения алгоритма растет пропорционально порядковому номеру простого числа.
+
+
+
+def test_value(func):
+    lst = [-1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    for i, item in enumerate(lst):
+        assert item == func(i)
+        print(f'Test {i} OK')
+    assert func(MAX_LIMIT_NUMBER) == -1
+    print(f'Test {MAX_LIMIT_NUMBER} OK')
+    assert func(9999) == 104723
+    print('Test 9999 OK')
+
+
+# test_value(no_sieve)
+# test_value(sieve)
+#
+# print(sieve(9999) == no_sieve(9999))
+# print(no_sieve(9999))
+# print(sieve(9999))
